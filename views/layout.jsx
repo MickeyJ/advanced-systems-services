f39dd67af29ff9f776f0fa9b93d50cd4a0ce13b2
MickeyJ <develop.agility@gmail.com>
2016-07-22 15:57:10 -0600

    materialize dirname must be lowercase

--- views/layout.jsx
+++ views/layout.jsx
@@ -10,20 +10,11 @@ export default class Layout extends Component{
   render(){
     return(
       <html lang="en">
-
-      <head>
-        <title>{this.props.title} - {siteName}</title>
-        <meta name="description" content={this.props.description}/>
-        <meta name="HandheldFriendly" content="true" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-        <link rel="stylesheet" href="bower_components/Materialize/dist/css/materialize.min.css"/>
-        <link rel="stylesheet" href="css/main.css"/>
-        <script src="bower_components/jquery/dist/jquery.min.js"></script>
-        <script src="bower_components/Materialize/dist/js/materialize.min.js"></script>
-        <script src="js/script.js"></script>
-      </head>
-        
+        <Head 
+          siteName={siteName}
+          title={this.props.title}
+          description={this.props.description}
+        />
         <body>
 
           <div id="all-wrap">
